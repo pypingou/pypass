6d36c428c7527ceb8b23277c2d66b5576f3767a4
trashy <johan@x-tnd.be>
2011-02-19 00:02:53 +0100

    Add license header to files ; fixes #108

--- pypass.py
+++ pypass.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+# Copyright (c) 2011 Pierre-Yves Chibon <pingou AT pingoured DOT fr>
+# Copyright (c) 2011 Johan Cwiklinski <johan AT x-tnd DOT be>
+#
+# This file is part of pypass.
+#
+# pypass is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pypass is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pypass.  If not, see <http://www.gnu.org/licenses/>.
+
 import pypass
 from optparse import OptionParser
 
