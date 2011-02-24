576211c4ce19c9ba568f228b4382c48b64789d20
Pierre-Yves Chibon <py@chibon.fr>
2011-02-24 18:08:14 +0100

    Directory disable, but let's still save the password

--- pypass/pyp.py
+++ pypass/pyp.py
@@ -92,8 +92,8 @@ class PyPass(object):
     def add_password(self, database, level, password):
         """ Add the given hashdict to the given database at the given
         level"""
-        if level is None:
-            database.passwords.append(password)
+        #if level is None:
+        database.passwords.append(password)
         #else:
             #database.directories[level].append(passdict)
         return database
